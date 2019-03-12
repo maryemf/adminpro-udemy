263804d6a05a1451bd7571fe88c82d8e952008ff
Mary Mayorga <emayorga@hidrocaven.com>
2019-03-13 00:26:31 +0100

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graph1Component } from './pages/graph1/graph1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
-import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
-
+import { RegisterComponent } from './login/register.component';
 
 // rutas
 import { APP_ROUTES } from './app.routes';
-import { PagesComponent } from './pages/pages.component';
-import { RegisterComponent } from './login/register.component';
+
+
+
+// modules
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    PagenotfoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graph1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
+    PagesModule,
     APP_ROUTES
   ],
   providers: [],
